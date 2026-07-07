6dbe2cdeb0b967760501af3a0937d02d52cd2193
Randa <randaerfan12@gmail.com>
2026-07-07 17:41:26 +0300

    update

--- config/cloudinary.config.js
+++
@@ -1,8 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-    cloud_name:'dngkblgyf',
-    api_key:'935138273125655',
-    api_secret:'ggJB5gxU1BEURwEypBjt5Gp-eKo'
-})
-module.exports = cloudinary
--- middleware/handleError.middleware.js
+++
@@ -1,9 +0,0 @@
-const handleError = (err, req, res, next) => {
-    const statuscode = err.statuscode || 500
-    const message = err.message || "Server Internal Error"
-    res.status(statuscode).json({
-        failed: true,
-        message: message
-    })
-}
-module.exports = handleError
